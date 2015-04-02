e13f00fc1646e3f455e7ebea62bf43b03f41d5e6
dylanleborgne <leborgne.dylan@gmail.com>
2015-04-02 23:49:43 +0200

    OpenVZ Install - Partie 2 - Configuration réseau en NAT - v1.0

--- Install_OpenVZ/Install_OpenVZ_ReseauNAT_Partie2_v1.0.sh
+++ Install_OpenVZ/Install_OpenVZ_ReseauNAT_Partie2_v1.0.sh
@@ -51,8 +51,8 @@ apt-get install iptables
 ip=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'` 
 
 echo -e "\n\n\n\033[31mVoulez pourvoir recevoir du ping ? (O/n): \033[0m"; read ping;
-echo -e "\n\n\033[31mUtilisez-vous le protole ssh ? (O/n): \033[0m"; read shh;
-	if [[ $ssh = "O" || $shh = "o" ]]; then
+echo -e "\n\n\033[31mUtilisez-vous le protole ssh ? (O/n): \033[0m"; read ssh;
+	if [[ $ssh = "O" || $ssh = "o" ]]; then
 		echo -e "\nQuel port utilisez-vous ? ('22', '2222'): \033[0m"; read ssh2;
 	fi
 
