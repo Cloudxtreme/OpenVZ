9ad6d3813a9c9891db341d634a4b62a572647037
dylanleborgne <leborgne.dylan@gmail.com>
2015-04-03 14:35:04 +0200

    OpenVZ Install - Partie 1 - Installation de l’environnement - v1.0

--- Install_OpenVZ/Partie1_Install_OpenVZ_v1.0.sh
+++ Install_OpenVZ/Partie1_Install_OpenVZ_v1.0.sh
@@ -19,6 +19,9 @@ apt-get install linux-image-openvz-amd64
 
 noyau=$(uname -r)
 
+echo -e "\033[31mSuppression du noyau, veuillez choisir non sur l'ecran suivant\033[0m"
+read -p "Appuyer sur une touche pour continuer ..."
+
 apt-get remove $noyau
 
 sed '/net.ipv4.ip_forward = 1/d' /etc/sysctl.conf
