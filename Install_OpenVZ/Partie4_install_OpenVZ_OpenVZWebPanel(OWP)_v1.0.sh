b48b3a7bf9d5c8e3855ad171d0157efa343b71aa
dylanleborgne <leborgne.dylan@gmail.com>
2015-04-03 15:11:11 +0200

    OpenVZ Install - Partie 4 - Installation de OpenVZ Web Panel (OWP) - v1.0

--- Install_OpenVZ/Partie4_install_OpenVZ_OpenVZWebPanel(OWP)_v1.0.sh
+++ Install_OpenVZ/Partie4_install_OpenVZ_OpenVZWebPanel(OWP)_v1.0.sh
@@ -13,6 +13,8 @@ wget -O - http://ovz-web-panel.googlecode.com/svn/installer/ai.sh | sh
 
 /etc/init.d/owp reload
 
+apt-get install ruby1.8
+
 ln -fs /usr/bin/ruby1.8 /etc/alternatives/ruby
 
 wget -O - http://ovz-web-panel.googlecode.com/svn/installer/ai.sh | sh
